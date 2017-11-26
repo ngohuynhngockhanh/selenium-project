3bf2a3a64e827db6c0da528549313b7feeda6655
Tuan <manganecchi@gmail.com>
2017-11-27 00:22:49 +0700

    add follow.js

--- test/follow.js
+++ test/follow.js
@@ -9,34 +9,48 @@ browser.timeouts('script', 10000);
 browser.timeouts('pageLoad', 10000);
 browser.timeouts('implicit', 10000);
 
-var testcases = loadjson(__dirname + "/database/search.json");
+var testcases = loadjson(__dirname + "/database/follow.json");
 
 describe('Test user login!\n', function() {
 	it('Check title must be: ksp | Cộng đồng Arduino Việt Nam', function() {
-		browser.url('/');
+		browser.url('http://arduino.vn/users/ksp');
 		assert.equal(browser.getTitle(), "ksp | Cộng đồng Arduino Việt Nam"); 
 	})
 	
 	_.map(testcases, function(testcase, index) {
 		it(`[#${index}] ${testcase.name}!`, function() {
 			// reload page for each test case
-			browser.url('/');
+			browser.url('http://arduino.vn/users/ksp');
 
-			// set value
-			var search_feild = $('#edit-search-block-form--2');
-			search_feild.setValue(testcase.search_text);
-			$("#search-block-form > div > div > button").click();
+			if (testcase.is_loggin.toString().trim() == "true") {
+				// click to show login form
+				$('#block-superfish-1 > ul > li:nth-child(1) > div').click();
 
-			// check result
-			var result = browser.getUrl();
-			assert.equal(result, testcase.expected_url);
-			result = browser.getHTML(".search-result", false)[0];
-			var start = result.indexOf('<a');
-			var end = result.indexOf('</a>'); 
-			result = result.substring(start, end);
-			result = result.substring(result.indexOf('>') + 1, result.length);
-			//console.log(result);
-			assert(result.indexOf(testcase.search_text) >= 0);
+				// set value
+				var name = $('#edit-name');
+				name.setValue(testcase.args.username);
+				var pass = $('#edit-pass');
+				pass.setValue(testcase.args.password);
+				$("#edit-submit--2").click();
+				
+				// check result
+				var result = browser.getUrl();
+				assert.equal(result, testcase.expected_result_url);
+
+				// get current state (follow or unfollow):
+				var follow_state = browser.getHTML('#main > div:nth-child(2) > div > div > div > div > div.padding10.cell.colspan3.rounded.article.full-article > div.profile > ul > li > span > span > a', false);
+				
+				// click to reverse state
+				$('#main > div:nth-child(2) > div > div > div > div > div.padding10.cell.colspan3.rounded.article.full-article > div.profile > ul > li > span > span').click();
+
+				// check new state
+				var new_follow_state = browser.getHTML('#main > div:nth-child(2) > div > div > div > div > div.padding10.cell.colspan3.rounded.article.full-article > div.profile > ul > li > span > span > a', false);
+				assert(follow_state.toString().trim() != new_follow_state.toString().trim());
+			} else {
+				// check does follow button exist
+				var follow_button = $('#main > div:nth-child(2) > div > div > div > div > div.padding10.cell.colspan3.rounded.article.full-article > div.profile > ul > li > span > span');
+				assert.equal(follow_button.state.toString().trim(), testcase.follow_button_state);
+			}
 		})
 	})
 });
